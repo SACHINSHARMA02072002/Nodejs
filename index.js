d51f63b24d236781d83e5e5f681283916f65b489
Sachin sharma <sachinsharma02072002@gmail.com>
2023-11-21 12:43:08 +0530

    NODEJS

---
+++ index.js
@@ -0,0 +1,14 @@
+// var a = require('one-liner-joke');
+
+// var b = a.getRandomJoke();
+// console.log(b)
+
+
+// var b = require('./hello');
+// console.log(b);
+
+
+
+var os = require('os');
+console.log(os.platform());
+console.log(os.arch());
